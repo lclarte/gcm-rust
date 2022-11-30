8ed1de7426f59b3e851ff13d8926afd98e61dcc7
Lucas Clarte <lucas.clarte@epfl.ch>
2022-11-30 19:45:26 +0100

    fixed normalisation issue with logit likelihood

--- src/gcmrust/data_models/logit.rs
+++ src/gcmrust/data_models/logit.rs
@@ -7,8 +7,7 @@ use crate::gcmrust::utility::constants::*;
 use super::super::channels::base_channel::Channel;
 use super::base_partition::{Partition, NormalizedChannel};
 
-
-static LOGIT_QUAD_BOUND : f64 = INTEGRAL_BOUNDS; 
+static LOGIT_QUAD_BOUND : f64 = 10.0; 
 
 #[pyclass(unsendable)]
 pub struct Logit {
@@ -24,41 +23,22 @@ fn noisy_sigmoid_likelihood(z : f64, noise_std : f64) -> f64 {
 
 impl Partition for Logit {
     fn z0(&self, y : f64, w  : f64, v : f64) -> f64 {
-        if self.noise_variance < 10.0_f64.powi(-10) {
-            let integrand = |z : f64| -> f64 {logistic::logistic(y * (z * v.sqrt() + w)) * (- z*z / 2.0).exp()};
-            return integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI).sqrt();
-        }
-        else {
-            let integrand = |z : f64| -> f64 { noisy_sigmoid_likelihood(y * (z * v.sqrt() + w), self.noise_variance.sqrt()) * (- z*z / 2.0).exp()};
-            return integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI).sqrt();
-        }
-        
+        let integrand = |z : f64| -> f64 { noisy_sigmoid_likelihood(y * (z * v.sqrt() + w), self.noise_variance.sqrt()) * (- z*z / 2.0).exp()};
+        return integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI).sqrt();    
     }
 
     fn dz0(&self, y : f64, w  : f64, v : f64) -> f64 {
-        if self.noise_variance < 10.0_f64.powi(-10) {
-            let integrand = |z : f64| -> f64 { z * logistic::logistic(y * (z * v.sqrt() + w)) * (- z*z / 2.0).exp()  };
-            return integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER))/ (2.0 * PI * v).sqrt();
-        }
-        else {
-            let integrand = |z : f64| -> f64 { z * noisy_sigmoid_likelihood(y * (z * v.sqrt() + w), self.noise_variance.sqrt()) * (- z*z / 2.0).exp()};
-            return integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI * v).sqrt();
-        }
+        let integrand = |z : f64| -> f64 { z * noisy_sigmoid_likelihood(y * (z * v.sqrt() + w), self.noise_variance.sqrt()) * (- z*z / 2.0).exp()};
+        return integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI * v).sqrt();
     }
 
     fn ddz0(&self, y : f64, w : f64, v : f64) -> f64 {
-        if self.noise_variance < 10.0_f64.powi(-10) {
-            let integrand = |z : f64| -> f64 { (z * z) * logistic::logistic(y * (z * v.sqrt() + w)) * (- z*z / 2.0).exp()  };
-            let integrale = integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER))/ (2.0 * PI * v).sqrt();
-            let z0 = self.z0(y, w, v);
-            return - z0 / v + integrale / v;
-        }
-        else {
-            let integrand = |z : f64| -> f64 { (z * z) * noisy_sigmoid_likelihood(y * (z * v.sqrt() + w), self.noise_variance.sqrt()) * (- z*z / 2.0).exp()};
-            let integrale = integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI * v).sqrt();
-            let z0 = self.z0(y, w, v);
-            return - z0 / v + integrale / v;
-        }
+        let integrand = |z : f64| -> f64 { (z * z) * noisy_sigmoid_likelihood(y * (z * v.sqrt() + w), self.noise_variance.sqrt()) * (- z*z / 2.0).exp()};
+        // below it was ... / (2.0 * PI).sqrt();
+        let integrale = integral::integrate(integrand, (-LOGIT_QUAD_BOUND, LOGIT_QUAD_BOUND), integral::Integral::G30K61(GK_PARAMETER)) / (2.0 * PI).sqrt();
+        let z0 = self.z0(y, w, v);
+        return - z0 / v + integrale / v;
+        
     }
 
 }
